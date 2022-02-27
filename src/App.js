7c16cfab496074a491da222a675ef3acb24b98db
wangy562 <andyyqwang@outlook.com>
2022-02-26 16:48:02 -0800

    fix bugs with wish 1

--- src/App.js
+++ src/App.js
@@ -5,35 +5,73 @@ import WishMulti from "./WishMulti"
 class App extends Component {
   constructor() {
     super(); 
-    this.state = { "pity": 0, "fs": 0, wishCount: 0 };
+    this.state = { "pity": 0, "fs": 0, wishType: 0, isLoaded: false, wishResults: [], error: null };
   }
 
-  summon = () => {
-    const updatedPity = this.state.pity + 1;
-    const updatedFs = this.state.fs + 1;
-    const wishCount = 1;
-    this.setState({ "pity": updatedPity, "fs": updatedFs, wishCount: wishCount })
+  summon = async () => {
+    const curPity = this.state.pity;
+    const curFs = this.state.fs; 
+    await this.getWishResultOne(curPity, curFs);
+    const updatedPity = this.computePity(curPity, curFs);
+    this.setState({ "pity": updatedPity.pity, "fs": updatedPity.fs, wishType: 1 });
   }
 
   summonMulti = () => {
+    const curPity = this.state.pity;
+    const curFs = this.state.fs;
     const updatedPity = this.state.pity + 10;
     const updatedFs = this.state.fs + 10;
-    const wishCount = 2;
-    this.setState({ "pity": updatedPity, "fs": updatedFs, wishCount: wishCount })
+    this.setState({ "pity": updatedPity, "fs": updatedFs, wishType: 2 })
   }
 
-  tableDataChooser = () => {
-    const wishCount = this.state.wishCount;
-    if (wishCount === 0) {
+  getWishResultOne = async (pity, fs) => {
+    const baseUri = "https://summon-simulator-api.herokuapp.com/api/permBanner/";
+    const res = await fetch(`${baseUri}summon/?pity=${pity}&fs=${fs}`); 
+    const resBody = await res.json(); 
+    this.setState({ isLoaded: true, wishResults: [resBody] });
+  }
+
+  computePity = (pity, fs) => {
+    const wishResults = this.state.wishResults; 
+    console.log(wishResults)
+    let curPity = pity;
+    let curFs = fs;
+    for (const wishItemIndex in wishResults) {
+      const wishItem = wishResults[wishItemIndex]; 
+      const rarity = wishItem.rarity;
+      console.log(rarity);
+      if (rarity === 5) {
+        curPity = 0;
+        curFs++; 
+      } else if (rarity === 4) {
+          curPity++;
+          curFs = 0;
+      } else {
+          curPity++;
+          curFs++;
+      }
+    }
+    console.log({ pity: curPity, fs: curFs })
+    return { pity: curPity, fs: curFs }
+  }
+
+  chooseTableData = () => {
+    const wishType = this.state.wishType;
+    if (wishType === 0) {
       return <tr></tr>; 
     } else {
-      return (wishCount === 1 ? 
-      <WishOne pity={this.state.pity} fs={this.state.fs} /> : <WishMulti pity={this.state.pity} fs={this.state.fs}/>);
+        return (wishType === 1 ? 
+          <WishOne pity={this.state.pity} 
+                   fs={this.state.fs} 
+                   isLoaded={this.state.isLoaded}
+                   error={this.state.error} 
+                   wishItem={this.state.wishResults[0]}/> : 
+          <WishMulti pity={this.state.pity} fs={this.state.fs}/>);
     }
   }
 
-  render() {
-    const chosenTableData = this.tableDataChooser(); 
+  render() { 
+    const tableData = this.chooseTableData();
     return (<div>
               <button onClick={this.summon}>Wish X1</button> <button onClick={this.summonMulti}>Wish X10</button>
               <table border="1">
@@ -46,7 +84,7 @@ class App extends Component {
                   <tr>
                     <th>Name</th><th>Rarity</th>
                   </tr>
-                  {chosenTableData}
+                    {tableData}
                 </tbody>
               </table>
             </div>); 
--- src/WishOne.js
+++ src/WishOne.js
@@ -4,46 +4,21 @@ import React, { Component } from "react";
 class WishOne extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            item: {}
-        };
     }
 
-    componentDidMount() {
-        fetch(`https://summon-simulator-api.herokuapp.com/api/permBanner/summon?pity=${this.props.pity}&fs=${this.props.fs}`)
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                item: result
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-            });
-        });
-    }
-    
     render() {
-    const { error, isLoaded, item } = this.state;
-    if (error) {
-        return <tr><td>Error: {error.message}</td></tr>;
-        } else if (!isLoaded) {
-            return (<tr><td>Waiting...</td></tr>);
-        } else {
-            return (
-                    <tr>
-                        <td>{item.name}</td>
-                        <td>{item.rarity}</td>
-                    </tr>
-            );
+        const { error, isLoaded, wishItem } = this.props
+        if (error) {
+            return <tr><td>Error: {error.message}</td></tr>;
+            } else if (!isLoaded) {
+                return (<tr><td>Waiting...</td></tr>);
+            } else {
+                return (<tr>
+                          <td>{wishItem.name}</td>
+                          <td>{wishItem.rarity}</td>
+                        </tr>);
+            }
         }
     }
-}
 
 export default WishOne;
